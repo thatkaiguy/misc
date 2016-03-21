39dc193d98428a08c9f82cdc80faab5ad3e3723a
thatkaiguy <kai438@gmail.com>
2016-03-20 19:37:04 -0700

    merge sort js

---
+++ practice-questions/js/merge-sort.js
@@ -0,0 +1,37 @@
+// input: array of numbers
+// output: the input array sorted from least to greatest
+
+function mergeSort(arr) {
+  arr = arr || [];
+  if (arr.length < 2) return arr;
+
+  var mid = Math.floor( arr.length / 2 );
+  var left = mergeSort( arr.slice( 0, mid ) );
+  var right = mergeSort( arr.slice( mid, arr.length ) );
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  var merged = [];
+
+  var leftIdx = 0;
+  var rightIdx = 0;
+
+  while ( left.length > 0 && right.length > 0 ) {
+    if ( left[ 0 ] < right[ 0 ] ) {
+      merged.push( left.shift() );
+    } else {
+      merged.push( right.shift() );
+    }
+  }
+
+  if ( left.length > 0 ) merged = merged.concat( left );
+  if ( right.length > 0 ) merged = merged.concat( right );
+
+  return merged;
+}
+
+// console.log( mergeSort( [5, 2, 7, 3, 9, 9, 10] ) );
+
+module.exports = mergeSort;
